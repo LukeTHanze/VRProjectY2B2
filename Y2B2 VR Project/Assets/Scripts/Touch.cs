35d5e7e4e8d61fd3c3968dd0b09187e60c19bf3e
LukeTHanze <ltobin2010@gmail.com>
2019-12-10 22:02:27 +0100

    VR Basics
    Basics of VR setup for unity w/interactable objects

---
+++ Y2B2 VR Project/Assets/Scripts/Touch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour
+{
+
+    Renderer rend;
+
+    public Material result, start;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+
+        rend.material = start;
+    }
+
+    private void OnTriggerEnter(Collider other) // works with OnCollisionEnter also
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Touched box");
+
+            rend.material = result;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            rend.material = start;
+        }
+    }
+}
