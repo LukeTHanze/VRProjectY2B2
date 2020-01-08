d629d77138037656b5b0a36be84e8f7e8412b9df
Landerack <Robinvermeulenbedum@gmail.com>
2020-01-08 09:50:10 +0100

    ThePoolScene

---
+++ Y2B2 VR Project/Assets/Scripts/WaterSpinner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSpinner : MonoBehaviour
+{
+
+    void Update()
+    {
+       transform.Rotate(10f *Time.deltaTime, 0.0f, 0.0f, Space.Self);
+    }
+}
