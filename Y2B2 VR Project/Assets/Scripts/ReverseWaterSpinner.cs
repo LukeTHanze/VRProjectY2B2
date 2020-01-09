4ae8468710728ec3ad2e8b1b8aeddbb73d9ce1e0
Landerack <Robinvermeulenbedum@gmail.com>
2020-01-09 15:52:17 +0100

    PoolUpdates_Visual+reverseSpinScript

---
+++ Y2B2 VR Project/Assets/Scripts/ReverseWaterSpinner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverseWaterSpinner : MonoBehaviour
+{
+
+    void Update()
+    {
+       transform.Rotate(0, -2f * Time.deltaTime, 0, Space.Self);
+    }
+}
