b77f32ab1c0b37426083f98357563234dd55fe9d
LukeTHanze <ltobin2010@gmail.com>
2020-01-18 15:06:57 +0100

    Alpha Cutoff Slider

---
+++ Y2B2 VR Project/Assets/Scripts/PlaneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneManager : MonoBehaviour
+{
+    [Range(0.0f, 10.0f)]
+    public float speed;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Standard");
+    }
+
+    void Update()
+    {
+        float cutOffValue = Mathf.PingPong(Time.time / speed, 0.8f);
+        rend.material.SetFloat("_Cutoff", cutOffValue);
+    }
+}
